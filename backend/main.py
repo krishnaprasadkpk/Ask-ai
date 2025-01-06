59c11307495f8390d75e27271bbc0c925cb5e23a
krishnaprasadkpk <krishnaprasadk815@gmail.com>
2025-01-06 22:31:19 +0530

    middleware changed

--- backend/main.py
+++ backend/main.py
@@ -15,7 +15,8 @@ app=FastAPI(lifespan=lifespan)
 
 app.add_middleware(
         CORSMiddleware,
-        allow_origins=["http://localhost:3000"],
+        allow_origins=["http://localhost:3000",
+                       "https://ask-ai-backend.up.railway.app"],
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
